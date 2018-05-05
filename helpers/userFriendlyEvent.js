60dcafda875a199bd4c741e16263289d1daa78c1
Jason Athanasoglou <d-jmaster@hotmail.com>
2018-05-05 20:49:25 +0300

    small optimization for urls loop

--- helpers/userFriendlyEvent.js
+++ helpers/userFriendlyEvent.js
@@ -2,17 +2,18 @@ const getUrls = require('get-urls');
 
 function userFriendlyEvent(event) {
   const messageLines = [];
+  
   messageLines.push(`A new event just arrived! Topic: **${event.topic}**`);
 
   const urls = getUrls(JSON.stringify(event));
 
   if (urls.size !== 0) {
     messageLines.push(`Found links in the message:`);
-  }
 
-  urls.forEach(url => {
-    messageLines.push(`<${url}>`);
-  });
+    urls.forEach(url => {
+      messageLines.push(`<${url}>`);
+    });
+  }
 
   const message = messageLines.join('\n');
 
